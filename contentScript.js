a809fe0109a660ddfbd99fbaefaaf94970d1f74f
scodes73 <scodes73@gmail.com>
2023-06-07 20:50:34 +0530

    feat: Added localStorage for fetched subscriptions

--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,75 @@
-chrome.runtime.onMessage.addListener((obj, sender, response) => {
-  const { type } = obj;
-  if (type === "FETCH") {
+// const fetchBookmarks = () => {
+//   return new Promise((resolve) => {
+//     chrome.storage.sync.get("channels", (obj) => {
+//       resolve(obj["channels"] ? JSON.parse(obj[channels]) : []);
+//     });
+//   });
+// };
+
+// const addNewBookmarkEventHandler = async () => {
+//   const currentTime = youtubePlayer.currentTime;
+//   const newBookmark = {
+//     time: currentTime,
+//     desc: "Bookmark at " + getTime(currentTime),
+//   };
+// };
+
+// chrome.tabs.onUpdated.addListener(() => {
+//   console.log("updated");
+// });
+const channelNames = {};
+
+const ready = function (cb) {
+  document.readyState === "loading"
+    ? // The document is still loading
+      document.addEventListener("DOMContentLoaded", function (e) {
+        console.log("ccccccccccc");
+        cb();
+      })
+    : // The document is loaded completely
+      cb();
+};
+const fetchChannelsFromStorage = async () => {
+  await chrome.storage.local
+    .get("channels")
+    .then((val) => Object.assign(channelNames, val["channels"]));
+};
+const fetchChannelsToPopUp = (response) => {
+  fetchChannelsFromStorage();
+
+  if (Object.keys(channelNames).length !== 0) {
+    console.log("already  present");
+    response(channelNames);
+  } else {
+    console.log("already not present");
+
     const channels = Array.from(
       document.getElementsByTagName(`ytd-channel-renderer`)
     );
-    const channelNames = channels.reduce((previousObject, currentObject) => {
-      return Object.assign(previousObject, {
-        [currentObject.getElementsByTagName("yt-formatted-string")[1]
-          .innerText]: currentObject
-          .getElementsByTagName("a")[0]
-          .href.slice(25),
-      });
-    }, {});
-
+    console.log(channels);
+    Object.assign(
+      channelNames,
+      channels.reduce((previousObject, currentObject) => {
+        console.log(currentObject.getElementsByTagName("a")[0].href.slice(25));
+        return Object.assign(previousObject, {
+          [currentObject.getElementsByTagName("yt-formatted-string")[1]
+            .innerText]: currentObject
+            .getElementsByTagName("a")[0]
+            .href.slice(25),
+        });
+      }, {})
+    );
     console.log(channelNames);
-    response(channelNames);
+    chrome.storage.local.set({ channels: { ...channelNames } });
+  }
+  response(channelNames);
+};
+
+chrome.runtime.onMessage.addListener(async (obj, sender, response) => {
+  const { type } = obj;
+  if (type === "FETCH") {
+    fetchChannelsToPopUp(response);
   }
 });
+//   }
+// });
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 import { getActiveTabURL } from "./utils.js";
+const activeTab = await getActiveTabURL();
+const contentDivElement = document.getElementById("content");
+const actionsDivElement = document.getElementById("actions");
+// const formContent = document.getElementById("f1");
 function fetchVals() {
   console.log("aaa");
   chrome.tabs.sendMessage(
@@ -10,16 +14,58 @@ function fetchVals() {
   );
 }
 function updateVals(channels) {
-  console.log(channels);
-  contentDivElement.innerHTML = `Found ${Object.keys(channels)}`;
+  if (channels) {
+    contentDivElement.innerHTML = `Found ${
+      Object.keys(channels).length
+    } channels
+    <form id="f1"></form>`;
+    const formContent = document.getElementById("f1");
+    contentDivElement.append(formContent);
+
+    for (let i of Object.keys(channels)) {
+      addCheckbox(i, channels[i], formContent);
+    }
+  }
+  function addCheckbox(key, value, formContent) {
+    const checkbox = document.createElement("input");
+    const label = document.createElement("label");
+    checkbox.type = "checkbox";
+    checkbox.name = key;
+    checkbox.className = "channel-checkbox";
+    checkbox.value = value;
+    checkbox.id = `${key}-${value}`;
+    label.htmlFor = `${key}-${value}`;
+    label.textContent = value;
+    checkbox.addEventListener("change", () => {
+      console.log(checkbox.checked, checkbox.value);
+    });
+    formContent.append(checkbox);
+    formContent.append(label);
+  }
 }
 
-const activeTab = await getActiveTabURL();
-const actionsDivElement = document.getElementById("actions");
-const contentDivElement = document.getElementById("content");
-const btn = document.createElement("button");
-btn.textContent = "Fetch";
+// function addPage() {
+//   const btn = document.createElement("button");
+//   btn.textContent = "Fetch";
 
-actionsDivElement.append(btn);
+//   actionsDivElement.append(btn);
 
-btn.addEventListener("click", fetchVals);
+//   btn.addEventListener("click", fetchVals);
+// }
+
+if (document.readyState !== "loading") {
+  if (activeTab.url.includes("youtube.com/feed/channels")) {
+    // addPage();
+
+    fetchVals();
+
+    console.log("actual page");
+  } else {
+    if (!activeTab.url.includes("youtube.com/feed/channels")) {
+      const container = document.getElementsByClassName("container")[0];
+      container.innerHTML =
+        '<div class="title">This is not youtube\'s subscription page.</div>';
+    }
+  }
+}
+console.log(document.readyState);
